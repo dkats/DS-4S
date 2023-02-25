a0e7f6b700bc7cab05a489e4314999cb449384e6
Dan Kats <dkats12@gmail.com>
2023-02-25 14:59:09 -0500

    Added link to the survey form

--- js/header.js
+++ js/header.js
@@ -10,6 +10,7 @@ function header(level = 0) {
 	document.write('	<li class="homeclick" onclick="window.open(\'' + dir + 'index.html\',\'_self\')" id="header_home">Map</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'country.html\',\'_self\')" id="header_country">Country data</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'citation.html\',\'_self\')" id="header_citation">Publication and Citation Information</li>');
+	document.write('	<li onclick="window.open(\'https://redcap.partners.org/redcap/surveys/?s=NRPWHHH8HL\',\'_blank\')" id="header_citation">Fill out the Survey</li>');
 	document.write('</ul>');
 	document.write('</header>');
 }
