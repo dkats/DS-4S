98f62408a1817df5d77a1bde404736d54328e194
Dan Kats <dkats12@gmail.com>
2023-04-28 13:40:54 -0400

    Removed link to fill out survey

--- js/header.js
+++ js/header.js
@@ -9,7 +9,7 @@ function header(level = 0) {
 	document.write('<ul class="menu">');
 	document.write('	<li class="homeclick" onclick="window.open(\'' + dir + 'index.html\',\'_self\')" id="header_home">Map</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'country.html\',\'_self\')" id="header_country">Country data</li>');
-	document.write('	<li onclick="window.open(\'https://redcap.partners.org/redcap/surveys/?s=NRPWHHH8HL\',\'_blank\')" id="header_survey">Fill out the Survey</li>');
+	// document.write('	<li onclick="window.open(\'https://redcap.partners.org/redcap/surveys/?s=J3ANH9LRYEC8CLYK\',\'_blank\')" id="header_survey">Request Access to the Survey</li>');
 	document.write('	<li style="float:right;" onclick="window.open(\'' + dir + 'citation.html\',\'_self\')" id="header_citation">Publication and Citation Information</li>');
 	document.write('	<li style="float:right;" onclick="window.open(\'' + dir + 'about.html\',\'_self\')" id="header_about">About Us</li>');
 	document.write('</ul>');
