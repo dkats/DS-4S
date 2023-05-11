9eb4a47ecaafbe84ab68195d878b942cc6b3a5f3
Dan Kats <dkats12@gmail.com>
2023-05-11 10:44:42 -0400

    Added link to request access to the survey

--- js/header.js
+++ js/header.js
@@ -10,7 +10,7 @@ function header(level = 0) {
 	document.write('	<li class="homeclick" onclick="window.open(\'' + dir + 'index.html\',\'_self\')" id="header_home">DS-4S</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'map.html\',\'_self\')" id="header_map">Map</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'country.html\',\'_self\')" id="header_country">Country Data</li>');
-	// document.write('	<li onclick="window.open(\'https://redcap.partners.org/redcap/surveys/?s=J3ANH9LRYEC8CLYK\',\'_blank\')" id="header_survey">Request Access to the Survey</li>');
+	document.write('	<li onclick="window.open(\'https://redcap.partners.org/redcap/surveys/?s=J3ANH9LRYEC8CLYK\',\'_blank\')" id="header_survey">Request Access to the Survey</li>');
 	document.write('	<li style="float:right;" onclick="window.open(\'' + dir + 'citation.html\',\'_self\')" id="header_citation">Publication and Citation Information</li>');
 	document.write('	<li style="float:right;" onclick="window.open(\'' + dir + 'about.html\',\'_self\')" id="header_about">About Us</li>');
 	document.write('</ul>');
