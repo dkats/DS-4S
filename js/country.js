cb2e331f7629e5613ef4bc52e7c7660b62869056
dkats <dkats@users.noreply.github.com>
2024-07-12 08:48:36 -0600

    Update country.js

--- js/country.js
+++ js/country.js
@@ -135,7 +135,9 @@ function loadData() {
 			out += "<p class='question'>" + data_dictionary[i].question + "</p>";
 			out += (data_dictionary[i].scale === "null" ? "null" : "<p class='scale'>(Scale:&nbsp;&nbsp;&nbsp;&nbsp;" + data_dictionary[i].scale.replaceAll(/[0-9],/g, '').replaceAll('<br>', '').replaceAll(" | ","&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;") + ")</p>");
 			out += "<p class='response'>" + (data[data_dictionary[i].variable] === "null" ? "<span class='response_unanswered'>Not answered</span>" : data[data_dictionary[i].variable]) + "</p>";
-			out += "<div class='response_note'>" + (notes[data_dictionary[i].variable] === null ? "" : "Note: " + notes[data_dictionary[i].variable]) + "</div>";
+			if(notes != undefined) {
+				out += "<div class='response_note'>" + (notes[data_dictionary[i].variable] === null ? "" : "Note: " + notes[data_dictionary[i].variable]) + "</div>";
+			}
 			out += "</div>";
 		}
 		responses_el.innerHTML = out;
